739038fe03a368df8f48e569c62f9bd08e8ddd4c
nagualchat <nagualchat@yandex.ru>
2018-04-22 16:24:25 +0700

    relocate

--- backend/models/chapter.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const chapterSchema = new mongoose.Schema({
-  book: { id: String, title: String, author: String },
-  chapter: { id: Number, title: String },
-  paragraphs: mongoose.Schema.Types.Mixed
-})
-
-module.exports = mongoose.model("chapters", chapterSchema)
--- backend/models/query.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const querySchema = new mongoose.Schema({
-  date: { type: Date, default: Date.now },
-  string: String
-})
-
-module.exports =  mongoose.model("search_queries", querySchema)
