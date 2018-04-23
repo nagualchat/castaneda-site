9755e4a65d53cce47af6bd290a2ff838a6861302
nagualchat <nagualchat@yandex.ru>
2018-04-23 19:51:10 +0700

    _

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 var config = module.exports = {}
 
-if (process.env.NODE_ENV === 'production') {
-  config.selfHost = 'http://nagualchat.ml/'
+if (process.env.NODE_ENV === "production") {
+  config.selfHost = "http://nagualchat.ml/#/"
 } else {
-  config.selfHost = 'http://localhost:8080/'
+  config.selfHost = "http://0.0.0.0:8080/#/"
 }
 
 config.apiHost = "http://ec2-18-222-19-123.us-east-2.compute.amazonaws.com:8081/"
